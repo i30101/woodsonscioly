38cecdec641e947c3db9ecf25a6aa4fb6cc7f8d1
Andrew Kim <andrewkim229@gmail.com>
2023-06-15 21:01:33 -0400

    happyness

---
+++ js/live-background.js
@@ -0,0 +1,120 @@
+/**
+ * Version: 1.0.0, 15 June 2023
+ * Author: Andrew Kim
+ */
+
+// get canvas
+const canvas = document.getElementById("canvas");
+
+const dotRadius = 130;
+const dotColor = "#2e2e2e";
+const lineWidth = 1;
+const canvasColor = "black";
+
+const ctx = canvas.getContext("2d");
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+// clear canvas
+function clearCanvas() {
+    ctx.fillStyle = canvasColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+
+
+// adjust script parameters to match viewable window
+if(window.innerWidth > 767) {
+    canvas.width = window.innerWidth - 217;
+    canvas.height = window.innerHeight;
+}else {
+    canvas.width = window.innerWidth - 17;
+    canvas.height = window.innerHeight - 65;
+}
+
+
+// Point class
+class Point {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.radius = 3;
+        this.speed = 1;
+
+        this.dx = this.randD();
+        this.dy = this.randD();
+    }
+
+    randD() {
+        return (Math.random() - 0.5) * this.speed;
+    }
+
+
+    // draw on canvas
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = dotColor;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+
+    // change direction after collision
+    reverseDirection() {
+        this.dx = -this.dx;
+        this.dy = -this.dy;
+    }
+
+    
+    // collision behavior and line drawing
+    updateGraphics() {
+        // collision with wall
+        if(this.x + this.dx > canvas.width - this.radius || this.x + this.dx < this.radius) {
+            this.reverseDirection();
+        }
+        if(this.y + this.dy > canvas.height - this.radius || this.y + this.dy < this.radius) {
+            this.reverseDirection();
+        }
+
+        // collision with other points and line drawing
+        for(let point of points) {
+            if(point === this) continue;
+            let distance = Math.sqrt(Math.pow((point.x - this.x), 2) + Math.pow((point.y - this.y), 2));
+            if(distance < dotRadius) {
+                ctx.strokeStyle = dotColor;
+                ctx.lineWidth = lineWidth * (dotRadius - distance) / dotRadius;
+                ctx.moveTo(this.x, this.y);
+                ctx.lineTo(point.x, point.y);
+                ctx.stroke();
+            }
+            if(distance < this.radius + point.radius) {
+                this.reverseDirection();
+                break;
+            }
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
+
+
+// create and store points
+let points = [];
+for(let i = 0; i < 100; i++) {
+    points.push(new Point());
+}
+
+
+// animate all points
+function animateAll() {
+    clearCanvas();
+    points.forEach(point => {
+        point.updateGraphics();
+        point.draw();
+    });
+    requestAnimationFrame(animateAll);
+}
+
+animateAll();
--- js/team.js
+++ js/team.js
@@ -1,12 +1,5 @@
-$(document).ready(function() {
-    $('.click-to-expand').click(function() {
-      var imageSrc = $(this).attr('src');
-      var overlay = $('<div class="overlay"><img src="' + imageSrc + '"></div>');
-      overlay.click(function() {
-        $(this).remove();
-      });
-      $('#image-overlay').html(overlay);
-      overlay.appendTo('body');
-    });
-  });
-  
+states_23 = ["Hailey Pan", "Sophia Huh", "Zahra Ramakdawala", "Kevin Benoy", "Temm Phan", "Leo Lee", "Viet Huynh", "Matthew Lee", "Aaron Li", "Cyrus Collins", "Ben Hall", "Andrew Kim", "Lavanya Mahajan", "Austin Cui", "Ada Qin"]
+regionals1_23 = ["Christopher Hwang", "James Kim", "Rushil Punukollu", "Enzo Hiu", "Joseph Tso ", "Stephen Labys", "Emily Cui", "Boram Min", "Breanna Ngo", "Nick Tong ", "Ivy Zhao", "Catherine Han", "Andrew Zheng", "Brian Vu", "Andre Mao"]
+regionals2_23 = ["Christine Nguyen", "Alice Lee", "Srivi Komireddy", "Nick Siew", "Indra Ranjan", "Cassie Liu", "Morgan Altier", "Jason Yoder", "Sonan Sahgal", "Susan Wang Andera", "Ryanne Juntima", "Hannah Do", "Enoch Tan", "Shaheer Khan", "Yool Lim"]
+alternates_23 = ["Gio Park", "Khang Tran", "Austin Song", "Harun Khan", "Wynson Phan", "Alexander Choi", "Ryan Chong", "Richard He", "Trung Lam", "Allison Syron", "Ahmed Mohamed", "Jordan Hershaft", "Lien Nguyen", "Ariana Nafisi", "Meklit Demile"]
+
