b2d394b75986539fcb16769e1f6ad1319f755212
i30101 <andrewkim229@gmail.com>
2025-03-18 08:32:55 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @author Andrew Kim
+ * @version 3.0.0
+ * @since 14 March 2025
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// external libraries
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+} from 'react-router-dom'
+import {useEffect} from "react";
+import Aos from 'aos';
+
+
+//styling
+import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './scss/global.scss';
+
+
+// local
+import PageRegistry from "./page/PageRegistry";
+
+
+
+// activate animate on scroll library
+const useAos = () => {
+    useEffect(() => {
+        Aos.init({
+            duration: 600,
+            delay: 100,
+            once: true
+        })
+    })
 }
+
+
+/**
+ * Dynamically creates list of pages
+ * @constructor main app container
+ */
+const App: React.FC = () => {
+    useAos();
+
+    return <Router>
+        <Routes>
+            {PageRegistry.getPages().map((page, index) => (
+                <Route key={index} path={page.path} element={<page.components />} />
+            ))}
+            <Route path={"*"} element={<Navigate to={"/404"} />} />
+        </Routes>
+    </Router>
+}
+
 
 export default App;
