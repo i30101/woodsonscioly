e11b1b1f0632c68d11baa098af3924c718a8cf41
i30101 <andrewkim229@gmail.com>
2025-05-12 17:08:40 -0400

    blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,9 +6,7 @@
 
 
 import React from "react";
-
-
-
+import blogIndex from "../blogIndex.json";
 
 
 interface BlogContainerProps {
@@ -31,14 +29,16 @@ const BlogContainer: React.FC<BlogContainerProps> = ({title, date, image, catego
 
 const blog: React.FC = () => (
     <div className="primary-section row">
-        {/*<BlogContainer title={"2025 Mary Washington Regional Results"} date={"March 2nd, 2025"} image={`${process.env.PUBLIC_URL}/blog/fh2025.jpg`} category={"Competitions"} path={"./blog/fairfax2025"} />*/}
-        {/*<BlogContainer title={"2025 Princeton Invitational Results"} date={"February 3rd, 2025"} image={`${process.env.PUBLIC_URL}/blog/fh2025.jpg`} category={"Competitions"} path={"./blog/fairfax2025"} />*/}
-        {/*<BlogContainer title={"2025 Fairfax Invitational Results"} date={"January 12th, 2025"} image={`${process.env.PUBLIC_URL}/blog/fh2025.jpg`} category={"Competitions"} path={"./blog/fairfax2025"} />*/}
-        <BlogContainer title={"2025 Flint Hill Invitational Results"} date="December 17th, 2024" image={`${process.env.PUBLIC_URL}/blog/fh2025.jpg`} category="Competitions" path="/blog/fh-2025" />
-        <BlogContainer title={"2025 Monta Vista Invitational Results"} date="November 25th, 2024" image={`${process.env.PUBLIC_URL}/blog/mvso2025.jpg`} category="Competitions" path="/blog/mvso-2025"/>
-        <BlogContainer title={"2025 Georgia Scrimmage Results"} date="October 24th, 2024" image={`${process.env.PUBLIC_URL}/blog/georgia2025.jpg`} category="Competitions" path="/blog/georgia-2025"/>
-        <BlogContainer title={"September 2024 Newsletter"} date="September 10, 2024" image={`${process.env.PUBLIC_URL}/blog/september2024.jpg`} category="Newsletter" path="/blog/september-2024"/>
-        <BlogContainer title={"2023-2024: Season in Review"} date="June 12th, 2024" image={`${process.env.PUBLIC_URL}/blog/season2024.jpg`} category="Blog" path="/blog/season-2024"/>
+        {blogIndex.map((blog: any) => (
+            <BlogContainer
+                key={blog.slug}
+                title={blog.title}
+                date={blog.date}
+                image={`${process.env.PUBLIC_URL}${blog.image}`}
+                category={blog.category}
+                path={`/blog/${blog.slug}`}
+            />
+        ))}
     </div>
 );
 
